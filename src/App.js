c325b87187b5d35efcc8d0ef823f2de21e578f9d
Prateek Bahad <prateekbahad@gmail.com>
2024-08-19 22:59:14 +0530

    update change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./styles.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HeroSection from "./components/HeroSection/HeroSection";
+import HomePage from "./Pages/HomePage";
+import Cart from "./Pages/Cart";
+import Product from "./Pages/Product";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<HeroSection/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,50 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-        <div className="nav">
-            <p>Navbar</p>
+      <div className="navbar-section">
+        <div className="navbar-logo">
+          <NavLink to={"/"}>
+            <h1>E-Commerce</h1>
+          </NavLink>
         </div>
+
+        <div className="navbar-list">
+          <ul>
+            <Link to={"/"}>
+              <li>
+                <p> Home </p>
+              </li>
+            </Link>
+          </ul>
+          <ul>
+            <Link to={"/product"}>
+              <li>
+                <p> Product </p>
+              </li>
+            </Link>
+          </ul>
+          <ul>
+            <Link to={"/sign-up"}>
+              <li>
+                <p> Sign-Up </p>
+              </li>
+            </Link>
+          </ul>
+          <ul>
+            <Link to={"/cart"}>
+              <li>
+                <p> Cart </p>
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
