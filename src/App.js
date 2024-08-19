0722015f03d8046130b618eb68bf114c238b88f0
Prateek Bahad <prateekbahad@gmail.com>
2024-08-19 21:46:59 +0530

    update change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './styles.css';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import "./styles.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HeroSection/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <>
+    Hero Section
+    </>
+  )
+}
+
+export default HeroSection
