2e38b8a94884c6bda29d5813162dd71fc0f0522d
Prateek Bahad <prateekbahad@gmail.com>
2024-08-19 20:24:26 +0530

    updated change

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+        <div className="nav">
+            <p>Navbar</p>
+        </div>
+    </>
+  )
+}
+
+export default Navbar;
