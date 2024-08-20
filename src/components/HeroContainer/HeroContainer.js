f2e1388e974ca0f6aec6bb954f20239fa365686c
Prateek Bahad <prateekbahad@gmail.com>
2024-08-20 12:32:32 +0530

    update change

--- src/components/HeroContainer/HeroContainer.js
+++ src/components/HeroContainer/HeroContainer.js
@@ -4,7 +4,12 @@ import "./HeroContainer.css"
 const HeroContainer = () => {
   return (
     <>
-        
+        <div className="herocontainer">
+            <div className="introcontainer">
+                <h1>Welcome</h1>
+                <p>Mobile World</p>
+            </div>
+        </div>
     </>
   )
 }
