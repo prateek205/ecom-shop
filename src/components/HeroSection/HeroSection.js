abfcd2a67980dc06074788a9bded98d1c9075b8d
Prateek Bahad <prateekbahad@gmail.com>
2024-08-21 12:02:12 +0530

    update changes

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,14 +2,15 @@ import React from 'react'
 import "./HeroSection.css";
 import HeroContainer from '../HeroContainer/HeroContainer';
 import FeatureSection from '../FeatureSection/FeatureSection';
+import ServiceSection from '../ServiceSection/ServiceSection';
 
 const HeroSection = () => {
   return (
     <>
       <div className="heroSection_layout">
           <HeroContainer/>
-          <FeatureSection/>
-          
+          {/* <FeatureSection/> */}
+          <ServiceSection/>
       </div>
     </>
   )
---
+++ src/components/ServiceSection/ServiceSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./ServiceSection.css";
+
+const ServiceSection = () => {
+  return (
+    <>
+      <div className="service_container">
+        <div className="service_shipping">
+          <div className="service_details">
+            <i className="fas fa-truck"></i>
+            <p>Super Fast and Free Delivery</p>
+          </div>
+        </div>
+
+        <div className="service_contact">
+          <div className="service_non-contact-details">
+            <i className="fa-solid fa-shield-halved"></i>
+            <p>Non-Contact Shipping</p>
+          </div>
+          <div className="service_assured">
+            <i className="fa-solid fa-money-check"></i>
+            <p>100% MoneyBack Guaranteed</p>
+          </div>
+        </div>
+
+        <div className="service_payment">
+          <div className="service_pay">
+            <i class="fa-solid fa-credit-card"></i>
+            <p>Secured Payment Service</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ServiceSection;
