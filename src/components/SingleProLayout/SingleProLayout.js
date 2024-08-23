4124697086e74a199d7fd16a33ba48520f7daad5
Prateek Bahad <prateekbahad@gmail.com>
2024-08-23 20:13:17 +0530

    update change

--- src/components/SingleProLayout/SingleProLayout.js
+++ src/components/SingleProLayout/SingleProLayout.js
@@ -1,10 +1,265 @@
-import React from 'react'
-import "./SingleProLayout.css"
+import React from "react";
+import "./SingleProLayout.css";
 
 const SingleProLayout = () => {
   return (
-    <div>SingleProLayout</div>
-  )
-}
+    <>
+      <div className="singleprocontainer">
+        <div className="singleprolayout">
+          <div className="singleproimage">
+            <div className="singleprosideimg">
+              <div className="singleimglist">
+                <img
+                  src="https://m.media-amazon.com/images/I/71zFRCcMS2L._SX679_.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://m.media-amazon.com/images/I/51I0ka2Cj8L._SX679_.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://m.media-amazon.com/images/I/712CBkmhLhL._SX679_.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://m.media-amazon.com/images/I/81BnjSLm2oL._SX679_.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://m.media-amazon.com/images/I/611FiI6xN2L._SX679_.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
 
-export default SingleProLayout
+            <div className="singleproimgbtn">
+              <div className="singleproimages">
+                <img
+                  src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/8/9/n/-original-imagtc6fyrstd4jm.jpeg?q=70"
+                  alt=""
+                />
+              </div>
+              <div className="singlebtnlist">
+                <div className="addbtn">
+                  <button>Add to Cart</button>
+                </div>
+                <div className="addbtn">
+                  <button>Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="singleprodetail">
+            <div className="singleproheading">
+              <h3>Apple iPhone 15 Plus (Black, 512 GB)</h3>
+            </div>
+
+            <div className="singlepriceextra">
+              <p>Extra ₹14901 off</p>
+            </div>
+            <div className="singlepricedetails">
+              <div className="singlemainprice">
+                <p>₹1,04,999</p>
+              </div>
+              <div className="singleactualprice">
+                <p>₹1,19,900</p>
+              </div>
+              <div className="singlediscount">
+                <p>12% off</p>
+              </div>
+            </div>
+
+            <div className="singlewarrentydetails">
+              <div className="singlewarrentyimg">
+                <img
+                  src="https://rukminim2.flixcart.com/image/160/160/prod-fk-cms-brand-images/9d5696196cfb3f4440ca99b1018c8ff91a53716d1948ba73ee3bb68f36571d7a.jpg?q=90"
+                  alt=""
+                />
+              </div>
+              <div className="singlewarrentyheading">
+                <p>
+                  1 Year Manufacturer Warranty for Device and 6 Months
+                  Manufacturer Warranty for In-Box Accessories
+                </p>
+              </div>
+            </div>
+
+            <div className="singlecolorsdetails">
+              <div className="singlecolorheading">
+                <p>Colors</p>
+              </div>
+              <div className="singlecolorlist">
+                <img
+                  src="https://m.media-amazon.com/images/I/71zFRCcMS2L._SX679_.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://m.media-amazon.com/images/I/71PjpS59XLL._SX679_.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://m.media-amazon.com/images/I/71ZP6U9sWTL._SX679_.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div className="singlestoragedetails">
+              <div className="singlestorageheading">
+                <p>Storage</p>
+              </div>
+              <div className="singlestoragelist">
+                <p>128 GB</p>
+                <p>256 GB</p>
+                <p>512 GB</p>
+              </div>
+            </div>
+
+            <div className="singlehighlightdetails">
+              <div className="singlehighlightsheading">
+                <p>Highlights</p>
+              </div>
+              <div className="singlehighlightslist">
+                <ul>
+                  <li>
+                    <p>512 GB ROM</p>
+                  </li>
+                  <li>
+                    <p>17.02 cm (6.7 inch) Super Retina XDR Display</p>
+                  </li>
+                  <li>
+                    <p>48MP + 12MP | 12MP Front Camera</p>
+                  </li>
+                  <li>
+                    <p>A16 Bionic Chip, 6 Core Processor Processor</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="singledescrpdetails">
+              <div className="singledecrheading">
+                <p>Description</p>
+              </div>
+              <div className="singledescdetailing">
+                <p>
+                  Experience the iPhone 15 Plus – your dynamic companion.
+                  Dynamic Island ensures you stay connected, bubbling up alerts
+                  seamlessly while you're busy. Its durable design features
+                  infused glass and aerospace-grade aluminum, making it
+                  dependable and resistant to water and dust. Capture life with
+                  precision using the 48 MP Main Camera, perfect for any shot.
+                  Powered by the A16 Bionic Processor, it excels in
+                  computational photography and more, all while conserving
+                  battery life. Plus, it's USB-C compatible, simplifying your
+                  charging needs. Elevate your tech game with the iPhone 15 Plus
+                  – innovation at your fingertips. Goodbye cable clutter, hello
+                  convenience.
+                </p>
+              </div>
+            </div>
+
+            <div className="singleprodesc">
+              <div className="singleprodheading">
+                <p>Product Descriptions</p>
+              </div>
+              <div className="singleproductdetails">
+                <div className="singleproductsdetailings">
+                  <div className="singleproddetails">
+                    <p>Dynamic Island</p>
+                  </div>
+                  <div className="singleproddescriptions">
+                    <p>
+                      Dynamic Island bubbles up alerts and Live Activities — so
+                      you don’t miss them while you’re doing something else. You
+                      can track your next ride, see who’s calling, check your
+                      flight status, and so much more.
+                    </p>
+                  </div>
+                </div>
+                <div className="singleproductimages">
+                  <img src="https://rukminim2.flixcart.com/image/200/200/cms-rpd-img/2a68bc53a5b740068a62b8140a19770e_18a8cada497_1.jpeg?q=90" alt="" />
+                </div>
+              </div>
+
+              <hr />
+
+              <div className="singleproductdetails">
+                <div className="singleproductimages">
+                  <img src="https://rukminim2.flixcart.com/image/200/200/cms-rpd-img/af0f9e1e23c042ce9d8f27b02bb996d3_18a8cacbaad_HighlyDurable.jpeg?q=90" alt="" />
+                </div>
+                <div className="singleproductsdetailings">
+                  <div className="singleproddetails">
+                    <p>Highly Durable</p>
+                  </div>
+                  <div className="singleproddescriptions">
+                    <p>
+                    The innovative new design features back glass that has color infused throughout the material. A custom dual ion-exchange process for the glass, and an aerospace-gradealuminum enclosure, help make the iPhone 15 Plus incredibly durable. Dependably durable. The Ceramic Shield front is tougher than any smartphone glass. Moreover, the iPhone is splash, water, and dust resistant. What a relief.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              
+              <hr />
+
+              <div className="singleproductdetails">
+                <div className="singleproductsdetailings">
+                  <div className="singleproddetails">
+                    <p>48 MP Main Camera</p>
+                  </div>
+                  <div className="singleproddescriptions">
+                    <p>
+                    Now the Main camera shoots in super-high resolution. So it’s easier than ever to take standout photos with amazing detail — from snapshots to stunning landscapes.
+                    </p>
+                  </div>
+                </div>
+                <div className="singleproductimages">
+                  <img src="https://rukminim2.flixcart.com/image/200/200/cms-rpd-img/884d4a0dcc7142fa999639ee0083bdfa_18a8cacd60b_Camera.jpeg?q=90" alt="" />
+                </div>
+              </div>
+
+              <hr />
+
+              <div className="singleproductdetails">
+                <div className="singleproductimages">
+                  <img src="https://rukminim2.flixcart.com/image/200/200/cms-rpd-img/0d6d81be7460430c8afb23a71743c5bb_18a8cad00ae_Chip.jpeg?q=90" alt="" />
+                </div>
+                <div className="singleproductsdetailings">
+                  <div className="singleproddetails">
+                    <p>A16 Bionic Processor</p>
+                  </div>
+                  <div className="singleproddescriptions">
+                    <p>
+                    A16 Bionic powers all kinds of advanced features. Like computational photography used for 24 MP photos and next-gen portraits. Voice Isolation for phone calls. And smooth performance for graphics-intensive games. All with incredible efficiency for great battery life. No wonder it started as a Pro chip.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              
+              <hr />
+
+              <div className="singleproductdetails">
+                <div className="singleproductsdetailings">
+                  <div className="singleproddetails">
+                    <p>USB-C Compatible</p>
+                  </div>
+                  <div className="singleproddescriptions">
+                    <p>
+                    The new USB-C connector lets you charge your Mac or iPad with the same cable you use to charge your iPhone 15 Plus. You can even use the iPhone 15 Plus to charge the Apple Watch or AirPods. Bye-bye, cable clutter.
+                    </p>
+                  </div>
+                </div>
+                <div className="singleproductimages">
+                  <img src="https://rukminim2.flixcart.com/image/200/200/cms-rpd-img/1daf65ddb5a54589a1c3e3e27220e51c_18a8cad20b5_USB.jpeg?q=90" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleProLayout;
