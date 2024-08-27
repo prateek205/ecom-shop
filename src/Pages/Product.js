462d5c129fbad1a9c31f9ec16344dc41cbd97026
Prateek Bahad <prateekbahad@gmail.com>
2024-08-27 20:59:50 +0530

    update change

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Products from '../components/Products/Products'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <>
+      <Products/>
+    </>
   )
 }
 
---
+++ src/components/Products/Products.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Products.css"
+
+const Products = () => {
+  return (
+    <>
+        <div className="productscontainer">
+            <div className="productsfilter">
+                filter
+            </div>
+            <div className="productsdetails">
+                <div className="productssort">
+                    sort
+                </div>
+                <div className="productslist">
+                    products
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Products
