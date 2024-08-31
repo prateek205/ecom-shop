4929b10b9c612980b6a2c59bac4a9859fc85f640
Prateek Bahad <prateekbahad@gmail.com>
2024-08-31 22:51:40 +0530

    update change

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Login from '../components/Login/Login'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <>
+      <Login/>
+    </>
   )
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <>
+      Sign Up
+    </>
+  )
+}
+
+export default Login
